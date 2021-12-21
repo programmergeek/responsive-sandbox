b38984c777b1e87af4f40c0d31a094a228ef18e5
programmergeek <programergeek@gmail.com>
2021-12-21 20:27:12 +0200

    implement props

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export const Carousel: React.FC = () => {
+type carouselProps = {
+  buttonColour?: string | "none";
+  autoScroll?: boolean;
+  loop?: boolean;
+  hideNavigationButtons?: boolean;
+  hideIndicator?: boolean;
+  height?: string;
+};
+
+/**
+ * Displays elements passed in side by side. Elements to be displayed are passed in as child elements.
+ * The component is built with image elements in mind, other types of elements can be passed in to varying degrees of success.
+ *
+ */
+export const Carousel: React.FC<carouselProps> = ({ ...props }) => {
   return <div></div>;
 };
