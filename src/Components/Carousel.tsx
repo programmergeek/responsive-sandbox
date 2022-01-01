27b6276685faf85ff9c5ceaa9b8afbf00da9d2f3
programmergeek <programergeek@gmail.com>
2022-01-01 12:59:09 +0200

    create and implement the carousel navigation buttons

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -9,21 +9,57 @@ type carouselProps = {
   loop?: boolean;
 };
 
-const Slides: React.FC<{ elements: JSX.Element[]; currentSlide: number }> = ({
-  ...props
-}) => {
+const Slides: React.FC<{
+  elements: JSX.Element[];
+  currentSlide: number;
+  translateAmount: number;
+}> = ({ ...props }) => {
   return (
-    <div className="flex flex-nowrap h-1/2 ">
-      {props.elements.map((slide, key) => {
-        return (
-          <img
-            src={slide.props.src}
-            key={key}
-            alt="slide"
-            className="min-w-[75%] lg:min-w-[42%] mx-1 md:mx-5 self-center rounded-3xl"
-          />
-        );
-      })}
+    <div className="overflow-hidden h-1/2" id={"slides"}>
+      <div
+        className={`flex`}
+        style={{ transform: `translate(${props.translateAmount}px, 0)` }}
+      >
+        {props.elements.map((slide, key) => {
+          return (
+            <img
+              src={slide.props.src}
+              key={key}
+              alt="slide"
+              className="min-w-[75%] lg:min-w-[50%] mx-1 md:mx-2 self-center rounded-3xl"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const LeftNav: React.FC<{
+  setTranslateAmount: React.Dispatch<React.SetStateAction<number>>;
+}> = ({ ...props }) => {
+  return (
+    <div
+      onClick={() => {
+        props.setTranslateAmount((current) => current - 125);
+        console.log("hello");
+      }}
+    >
+      <IconButton buttonIcon={<ChevronLeft />} />
+    </div>
+  );
+};
+const RightNav: React.FC<{
+  setTranslateAmount: React.Dispatch<React.SetStateAction<number>>;
+}> = ({ ...props }) => {
+  return (
+    <div
+      onClick={() => {
+        props.setTranslateAmount((current) => current + 125);
+        console.log("hello");
+      }}
+    >
+      <IconButton buttonIcon={<ChevronRight />} />
     </div>
   );
 };
@@ -32,10 +68,17 @@ export const Carousel: React.FC<carouselProps> = ({ ...props }) => {
   const [disableLeftNav, updateDisableLeftNav] = useState(true);
   const [disableRightNav, updateDisableRightNav] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [translateAmount, setTranslateAmount] = useState(0);
   const slides = props.children?.valueOf() as JSX.Element[];
   return (
     <div>
-      <Slides elements={slides} currentSlide={currentSlide} />
+      <Slides
+        elements={slides}
+        currentSlide={currentSlide}
+        translateAmount={translateAmount}
+      />
+      <LeftNav setTranslateAmount={setTranslateAmount} />
+      <RightNav setTranslateAmount={setTranslateAmount} />
     </div>
   );
 };
